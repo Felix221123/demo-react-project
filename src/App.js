a8fb427bff670a81d920285e18d5c29739a28aeb
Felix <felixbaah47@gmail.com>
2024-02-09 13:02:51 +0000

    added the output directory

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { jsxDEV as _jsxDEV, Fragment as _Fragment } from "react/jsx-dev-runtime";
+const _jsxFileName = "/Users/felixbaah/Desktop/myprojects/React_Apps/demo-react-app/src/App.tsx";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+function App() {
+    const [count, setCount] = useState(0);
+    return (_jsxDEV(_Fragment, { children: [_jsxDEV("div", { children: [_jsxDEV("a", { href: "https://vitejs.dev", target: "_blank", children: _jsxDEV("img", { src: viteLogo, className: "logo", alt: "Vite logo" }, void 0, false, { fileName: _jsxFileName, lineNumber: 13, columnNumber: 11 }, this) }, void 0, false, { fileName: _jsxFileName, lineNumber: 12, columnNumber: 9 }, this), _jsxDEV("a", { href: "https://react.dev", target: "_blank", children: _jsxDEV("img", { src: reactLogo, className: "logo react", alt: "React logo" }, void 0, false, { fileName: _jsxFileName, lineNumber: 16, columnNumber: 11 }, this) }, void 0, false, { fileName: _jsxFileName, lineNumber: 15, columnNumber: 9 }, this)] }, void 0, true, { fileName: _jsxFileName, lineNumber: 11, columnNumber: 7 }, this), _jsxDEV("h1", { children: "Vite + React" }, void 0, false, { fileName: _jsxFileName, lineNumber: 19, columnNumber: 7 }, this), _jsxDEV("div", { className: "card", children: [_jsxDEV("button", { onClick: () => setCount((count) => count + 1), children: ["count is ", count] }, void 0, true, { fileName: _jsxFileName, lineNumber: 21, columnNumber: 9 }, this), _jsxDEV("p", { children: ["Edit ", _jsxDEV("code", { children: "src/App.tsx" }, void 0, false, { fileName: _jsxFileName, lineNumber: 25, columnNumber: 16 }, this), " and save to test HMR"] }, void 0, true, { fileName: _jsxFileName, lineNumber: 24, columnNumber: 9 }, this)] }, void 0, true, { fileName: _jsxFileName, lineNumber: 20, columnNumber: 7 }, this), _jsxDEV("p", { className: "read-the-docs", children: "Click on the Vite and React logos to learn more" }, void 0, false, { fileName: _jsxFileName, lineNumber: 28, columnNumber: 7 }, this)] }, void 0, true, { fileName: _jsxFileName, lineNumber: 9, columnNumber: 11 }, this));
+}
+export default App;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { jsxDEV as _jsxDEV } from "react/jsx-dev-runtime";
+const _jsxFileName = "/Users/felixbaah/Desktop/myprojects/React_Apps/demo-react-app/src/main.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+ReactDOM.createRoot(document.getElementById('root')).render(_jsxDEV(React.StrictMode, { children: _jsxDEV(App, {}, void 0, false, { fileName: _jsxFileName, lineNumber: 8, columnNumber: 5 }, this) }, void 0, false, { fileName: _jsxFileName, lineNumber: 6, columnNumber: 62 }, this));
